040755053443937ef34681e6f71f01d23ec67071
Florencia Pavon <florenciapavonbitar@gmail.com>
2025-05-22 01:37:34 -0300

    Add counter CartWidget

--- ProyectoFinal+Pavon/src/components/CartWidget/CartWidget.jsx
+++ ProyectoFinal+Pavon/src/components/CartWidget/CartWidget.jsx
@@ -1,8 +1,18 @@
+import { useContext } from "react"
+import {CarritoContext} from '../../context/CarritoContext'
+import { Link } from "react-router-dom"
 import "./CartWidget.css"
 
 const CartWidget = () => {
+  const {cantidadTotal} = useContext(CarritoContext);
   return (
-    <i className="bi bi-cart3"></i>   
+    <div className="cart-widget-container">
+      <Link to="/cart" className="cart-link">
+        <i className="bi bi-cart3 cart-icon"></i>
+        {cantidadTotal > 0 && <span className="cart-cantidad">{cantidadTotal}</span>}
+      </Link>
+    </div>
+
   )
 }
 
